20968af1084851b86144a08be808f854f1387f56
Candice Marx <candice.marx@hotmail.com>
2019-05-21 20:51:57 -0600

    end of lecture

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  has_many :notes, dependent: :destroy
+  #Associations
+  #Validations
+  #Class Method 
+  #Instance Method
+  has_one :address, dependent: :destroy
 end
